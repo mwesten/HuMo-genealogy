953ffd833d65eb6bd0dd03bc4a8e20c05d8db3e5
HuubMons <github@huubmons.nl>
2025-10-17 11:48:47 +0200

    Improved BotDetector

--- include/BotDetector.php
+++ include/BotDetector.php
@@ -12,6 +12,7 @@ class BotDetector
         // *** For testing purposes, simulate a bot user agent ***
         // $_SERVER['HTTP_USER_AGENT'] = 'bot';
 
-        return preg_match('/bot|spider|crawler|curl|Yahoo|Google|Bingbot|DuckDuckBot|Baiduspider|Yandex|Sogou|facebookexternalhit|Slackbot|Discordbot|Applebot|SemrushBot|AhrefsBot|MJ12bot|^$/i', $_SERVER['HTTP_USER_AGENT']) === 1;
+        $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+        return preg_match('/bot|spider|crawler|curl|Yahoo|Google|Bingbot|DuckDuckBot|Baiduspider|Yandex|Sogou|facebookexternalhit|Slackbot|Discordbot|Applebot|SemrushBot|AhrefsBot|MJ12bot|^$/i', $userAgent) === 1;
     }
 }
